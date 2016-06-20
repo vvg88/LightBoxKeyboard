3619ed24ca8916b026f427399adcec8c5e8a97aa
grishanin_vv <Grishanin_VV@Grishanin.neurosoft.ru>
2016-06-20 17:29:20 +0300

    Начальная фиксация. Создание проекта

---
+++ RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'fw_034_2015_keyboard' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
